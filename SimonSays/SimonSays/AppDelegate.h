24f49f6a69377568251fbc86aa500bcfe164cec4
aacohen <arielaades@gmail.com>
2016-09-21 10:56:05 -0400

    Swift Interface Builder - Simon Says Complete

---
+++ SimonSays/SimonSays/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimonSays
+//
+//  Created by Ariela Cohen on 9/21/16.
+//  Copyright © 2016 Ariela Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimonSays/SimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimonSays
+//
+//  Created by Ariela Cohen on 9/21/16.
+//  Copyright © 2016 Ariela Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
